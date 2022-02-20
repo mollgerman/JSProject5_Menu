75122487b86adeb6e7b486c31c05cf1b54599ee0
mollgerman <germandmoll2004@gmail.com>
2022-02-20 01:24:48 -0300

    Added QR share modal

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const links = document.querySelector(".nav-links");
+const quitModal = document.querySelector(".quit-modal");
 const toggle = document.getElementById("toggle");
+const qr = document.getElementById("qr");
+const qrModal = document.getElementById("qrmodal");
 let toggleState = "closed"
 toggle.addEventListener("click", function() {
     if (toggleState == "closed") {
@@ -11,6 +14,15 @@ toggle.addEventListener("click", function() {
         toggleState = "closed";
         links.classList.remove("show-links");
         toggle.classList.remove("transToggle");
-        
     }
 })  
+qr.addEventListener("click", function(){
+    if (!qrModal.classList.contains("modal-show")) {
+        qrModal.classList.add("modal-show");
+    }
+})
+quitModal.addEventListener("click", function(){
+    if (qrModal.classList.contains("modal-show")) {
+        qrModal.classList.remove("modal-show");
+    }
+})
