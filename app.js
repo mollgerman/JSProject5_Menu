2e13271b9eafa05489cb8a0a201ebdc3ee858eed
mollgerman <germandmoll2004@gmail.com>
2022-02-20 13:47:34 -0300

    Share options update

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const quitModal = document.querySelector(".quit-modal");
 const toggle = document.getElementById("toggle");
 const qr = document.getElementById("qr");
 const qrModal = document.getElementById("qrmodal");
+const shareButton = document.querySelector('.share-button');
+const shareOptions = document.querySelector('.shareOptions');
 let toggleState = "closed"
 toggle.addEventListener("click", function() {
     if (toggleState == "closed") {
         toggleState = "opened";
         links.classList.add("show-links");
+        shareOptions.classList.add("showShareOptions");
         toggle.classList.add("transToggle");
     } 
     else {
         toggleState = "closed";
         links.classList.remove("show-links");
         toggle.classList.remove("transToggle");
+        shareOptions.classList.remove("showShareOptions");
     }
 })  
 qr.addEventListener("click", function(){
@@ -26,3 +30,18 @@ quitModal.addEventListener("click", function(){
         qrModal.classList.remove("modal-show");
     }
 })
+
+
+shareButton.addEventListener('click', event => {
+    if (navigator.share) { 
+     navigator.share({
+        title: 'WebShare API Demo',
+        url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+      }).then(() => {
+        console.log('Thanks for sharing!');
+      })
+      .catch(console.error);
+      } else {
+          shareDialog.classList.add('is-open');
+      }
+  });
